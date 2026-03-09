38dd054cdca44b6e4c9ba3a5fc0ff58c2b66c267
Adeshina-Qudus <qudusa55@gmail.com>
2026-03-09 14:16:06 +0100

    little fixes

--- src/pages/gmailLogin/EmailForm.js
+++ src/pages/gmailLogin/EmailForm.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface EmailFormProps {
     email: string;
     onEmailChange: (value: string) => void;
@@ -5,7 +7,7 @@ interface EmailFormProps {
 }
 
 const EmailForm = ({ email, onEmailChange, onNext }: EmailFormProps) => (
-    <>
+    <div className="email-form-wrapper">
         <input
             type="text"
             placeholder="Email or phone"
@@ -20,24 +22,24 @@ const EmailForm = ({ email, onEmailChange, onNext }: EmailFormProps) => (
             Forgot email?
         </button>
 
-        <a
-            href="https://support.google.com/chrome/answer/6130773"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="learn-more-link"
+
+        <a href="https://support.google.com/chrome/answer/6130773"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="learn-more-link"
         >
-            Learn more
+        Learn more
         </a>
 
-        <div className="button-group">
-            <button type="button" className="create-account-btn">
-                Create account
-            </button>
-            <button type="button" className="next-btn" onClick={onNext}>
-                Next
-            </button>
-        </div>
-    </>
+    <div className="button-group">
+        <button type="button" className="create-account-btn">
+            Create account
+        </button>
+        <button type="button" className="next-btn" onClick={onNext}>
+            Next
+        </button>
+    </div>
+</div>
 );
 
 export default EmailForm;
